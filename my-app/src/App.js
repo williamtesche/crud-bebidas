a9b7cdf125fe4dfd334a584c9725956ea17ae3ee
Willian Matheus Ribeiro Tesche <85637800+williamtesche@users.noreply.github.com>
2021-10-26 19:21:43 -0300

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,41 @@
 import './App.css';
 import React from 'react';
-import Button  from './components/Button';
+import {useState} from 'react'; 
 
-const App =   () => {
+function App() {
+
+  let [lista,setLista] = useState([]);
+  let [novoitem,setNovoItem] = useState("");
+
+
+  let [listar,setListar] = useState([]);
+  let [segundoitem,setSegundoItem] = useState("");
+
+
+
   return ( 
     <div className="App">
         <h1>Bem vindo ao meu site</h1>
-        <input tipe="text" placeholder="Seu Nome"></input>
+
+        <input value={novoitem} onChange={value => setNovoItem(value.target.value)} type="text" placeholder="Nome"/>
         <br />
-        <input tipe="text" placeholder="Sua idade"></input>
-        <br/>
-        <Button>Adicionar</Button>
+        <input value={segundoitem} onChange={value => setSegundoItem(value.target.value)} type="text" placeholder="Idade"/>
+       <br />
+        <button onClick={() => adicionarNovoItem()}> adicionar</button>
+
+        {lista.map(item => <div>{item}</div>)}
 
      
     </div>
   );
 
+  function adicionarNovoItem() {
+    setLista([...lista, novoitem])
+  }
+
+  function adicionarSegundoItem() {
+    setListar([...listar, segundoitem])
+  }
 
 };
 
